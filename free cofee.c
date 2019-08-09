a92197d2fb390a599e9fb2e3736bb9c422f24f3c
tbkkrishna005 <53944475+tbkkrishna005@users.noreply.github.com>
2019-08-09 20:02:52 +0530

    Create free cofee.c

---
+++ free cofee.c
@@ -0,0 +1,15 @@
+#include<iostream.h>
+using namespace std;
+int main()
+{
+   int x,q,s=0;
+   cin>>x>>p;
+   q=100-q;
+   while(x>0)
+   {
+       s=s+x;
+       x=(q*x)/100;
+   }
+   cout<<s;
+   return 0;
+}
